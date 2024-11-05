e67d48042534678bbe25e369faf7f751b207205f
hejinjie <896731674@qq.com>
2024-11-05 22:26:39 +0800

    Mock服务代理

--- example-consumer/src/main/java/com/ayom/example/consumer/ComsumerExample.java
+++ example-consumer/src/main/java/com/ayom/example/consumer/ComsumerExample.java
@@ -1,11 +1,26 @@
 package com.ayom.example.consumer;
 
+import com.ayom.example.common.model.User;
+import com.ayom.example.common.service.UserService;
 import com.ayom.myrpc.config.RpcConfig;
+import com.ayom.myrpc.proxy.ServiceProxyFactory;
 import com.ayom.myrpc.utils.ConfigUtils;
 
 public class ComsumerExample {
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        //RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        //System.out.println(rpc);
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+
+        user.setName("hejinjie");
+        User user1 = userService.getUser(user);
+        if(user1 != null){
+            System.out.println(user1.getName());
+        }else{
+            System.out.println("user == null");
+        }
+        short number = userService.getNumber();
+        System.out.println(number);
     }
 }
--- my-rpc-core/src/main/java/com/ayom/myrpc/proxy/ServiceProxyFactory.java
+++ my-rpc-core/src/main/java/com/ayom/myrpc/proxy/ServiceProxyFactory.java
@@ -1,5 +1,7 @@
 package com.ayom.myrpc.proxy;
 
+import com.ayom.myrpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
@@ -14,10 +16,27 @@ public class ServiceProxyFactory {
      * @param <T>
      */
     public static <T> T getProxy(Class<T> serviceClass){
+        if(RpcApplication.getRpcConfig().isMock()){
+            return getMockProxy(serviceClass);
+        }
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},
                 new ServiceProxy()
+        );
+    }
+
+    /**
+     * 根据服务获取Mock代理对象
+     * @param serviceClass
+     * @return
+     * @param <T>
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass){
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy()
         );
     }
 }
